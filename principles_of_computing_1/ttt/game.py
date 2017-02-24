9e7c122173811b569bbf83d76ca39d5768b3a7a5
voidnologo <(none)>
2017-02-23 22:03:58 -0600

    monte carlo working (includes prints)

--- principles_of_computing_1/ttt/game.py
+++ principles_of_computing_1/ttt/game.py
@@ -1,5 +1,6 @@
 import collections
 import random
+import time
 
 
 class InvalidMoveException(Exception):
@@ -7,6 +8,7 @@ class InvalidMoveException(Exception):
 
 
 Player = collections.namedtuple('Player', ['name', 'moves'])
+MontePlayer = collections.namedtuple('MontePlayer', ['name', 'moves', 'new_moves'])
 
 
 class TTT():
@@ -70,21 +72,73 @@ class TTT():
 
 class MonteCarloTTT(TTT):
 
-    TRIALS = 100
+    TRIALS = 10
     SCORE_CURRENT = 1.0
     SCORE_OTHER = 1.0
 
-    def __init__(self, board):
-        self.initial_board = board
-        self.move_list = self.set_up_moves(self.inital_board)
+    def __init__(self, board, x_moves, o_moves):
+        self.x = MontePlayer('X', x_moves, [])
+        self.o = MontePlayer('O', o_moves, [])
+        self.current_player = self.x
+        self.initial_board = board[:]
+        self.move_list = self.set_up_moves()
+        self.scores = collections.defaultdict(int)
 
     def set_up_moves(self):
-        return random.sample(self.GRID_SPOTS, k=len(self.GRID_SPOTS))
+        return random.sample(self.initial_board, k=len(self.initial_board))
 
     def choose_spot(self, player):
-        player.moves.append(self.move_list.pop())
+        player.new_moves.append(self.move_list.pop())
+
+    def play(self):
+        for cnt, _ in enumerate(range(self.TRIALS)):
+            print('Trial:', cnt, '\n')
+            while self.move_list:
+                print()
+                self.choose_spot(self.current_player)
+                self.print_board()
+                if self.check_win(self.current_player.moves + self.current_player.new_moves):
+                    self.score_game()
+                    break
+                self.toggle_player()
+                time.sleep(1)
+            self.reset()
+        print('BEST MOVE:', self.best_move())
+        return self.best_move()
+
+    def reset(self):
+        del self.x.new_moves[:]
+        del self.o.new_moves[:]
+        self.move_list = self.set_up_moves()
+
+    def score_game(self):
+        if self.current_player is self.x:
+            self.score_moves(self.x.new_moves, self.SCORE_CURRENT)
+            self.score_moves(self.o.new_moves, -self.SCORE_OTHER)
+        else:
+            self.score_moves(self.o.new_moves, self.SCORE_OTHER)
+            self.score_moves(self.x.new_moves, -self.SCORE_CURRENT)
+        print('\nSCORES:', self.scores)
+
+    def score_moves(self, moves, value):
+        for spot in moves:
+            self.scores[spot] += value
+
+    def best_move(self):
+        high_score = max(self.scores.values())
+        return next((k for k, v in self.scores.items() if v == high_score))
+
+    def print_board(self):
+        def value(spot):
+            if spot in self.x.moves + self.x.new_moves: return self.x.name
+            if spot in self.o.moves + self.o.new_moves: return self.o.name
+            return spot
+        for row in zip(*[iter(self.GRID_SPOTS)] * 3):
+            print(' '.join([str(value(i)) for i in row]))
 
 
 if __name__ == '__main__':
-    game = TTT()
+    # game = TTT()
+    # game.play()
+    game = MonteCarloTTT([1, 2, 4, 7], [3, 5], [8, 9])
     game.play()
